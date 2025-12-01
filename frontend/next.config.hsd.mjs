4aac4e61f65d4ca74077c11ba332c6cbbe7002ef
Hien Giang <gianghien2999@gmail.com>
2025-12-01 11:54:05 +0100

    update with render setup

--- frontend/next.config.hsd.mjs
+++ frontend/next.config.hsd.mjs
@@ -4,13 +4,13 @@ const nextConfig = {
   images: { unoptimized: true },
   trailingSlash: true,
 
-  // NEW paths:
+  // new paths:
   basePath: '/2026-FBM-Web-KI-Chatbot',
   assetPrefix: '/2026-FBM-Web-KI-Chatbot/',
 
   env: {
     NEXT_PUBLIC_BASE_PATH: '/2026-FBM-Web-KI-Chatbot',
-    NEXT_PUBLIC_API_URL: 'https://rag-openai-pinecone.onrender.com'
+    NEXT_PUBLIC_API_URL: 'https://kim-chatbot.onrender.com'
   },
 };
 
